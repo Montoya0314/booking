1e42c83792cf286617aac7edaed5646f25dcf503
Montoya0314 <103960531+Montoya0314@users.noreply.github.com>
2022-09-10 12:10:23 -0500

    Add files via upload

--- Booking_Automation/src/main/java/com/booking/certification/devco/utils/EndPoints.java
+++ Booking_Automation/src/main/java/com/booking/certification/devco/utils/EndPoints.java
@@ -1,7 +1,7 @@
 package com.booking.certification.devco.utils;
 
 public class EndPoints {
-    public static final String BOOKING = "https://www.booking.com/";
+    public static final String BOOKING = "https://www.booking.com/index.en-gb.html?label=gen173nr-1DCAEoggI46AdIM1gEaDKIAQGYAQm4ARfIAQzYAQPoAQGIAgGoAgO4Ap-h75gGwAIB0gIkYmJkYjZlZGEtMmY3YS00MGJjLWE2MTUtMDZkY2Y3YWVhMzg22AIE4AIB&sid=016062360bb4ced0da50c1618f986597&keep_landing=1&sb_price_type=total&";
 
     private EndPoints() {
     }
